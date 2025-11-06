41eb746b9b73c3d4c6f4f1b4fc8e87f916145bb2
Emmanuel TAHI <emmanueltahi14@gmail.com>
2025-11-06 05:45:23 +0000

    Correction de légers bug avec l'api

--- Gestion_Cantine_HEG/urls.py
+++ Gestion_Cantine_HEG/urls.py
@@ -2,11 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("cantine_web.api_urls")),  # API REST pour Flutter
     path("", include("cantine_web.urls")),  # Interface web pour Admin
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
 
 if settings.DEBUG:
